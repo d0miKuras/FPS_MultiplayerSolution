b1570672c33136ef4be62b9711abe685734b4f61
d0miKuras <dominik.kurasbediani@gmail.com>
2021-09-17 20:08:11 +0200

    Init commit try 3
    This is my third reupload (git has been annoying or I'm just sleep deprived). TCP and UDP communications between client and server implemented.

---
+++ FPS_DedicatedServer/GameLogic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FPS_DedicatedServer
+{
+    public class GameLogic
+    {
+        public static void Update()
+        {
+            ThreadManager.UpdateMain();
+        }
+    }
+}
